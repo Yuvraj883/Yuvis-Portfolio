18100902ebb0cd9bcb73aadc8c7c47aa58dd9f0c
Yuvraj <kingyuvi88@gmail.com>
2023-04-10 00:39:17 +0530

    Added react icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
+// import {GiCrownOfThorns} from 'react-icon/gi'; 
+import {GiCrown, GiCrownOfThorns} from 'react-icons/gi'
+
 function Navbar(){
     return(
         <>
         <nav className="flex text-red-950 ">
-            <span className="m-2 p-2 font-bold font-serif text-xl">Dev-Yuvi</span>
+            <span className="m-2 p-2 font-bold font-serif text-xl text-yellow-500"><GiCrownOfThorns className="inline text-3xl"/> Dev-Yuvi</span>
             <ul className="flex ml-auto px-4 font-bold cursor-pointer">
                 <li className="m-2 p-2">Home</li>
                 <li className="m-2 p-2">About Me</li>
