b2e6c3fe9d2aa495dc4ce4b567576c94536d98b9
Yuvraj <kingyuvi88@gmail.com>
2023-04-09 23:20:14 +0530

    Created Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Navbar from './Components/Navbar'
 function App(){
   return(
     <>
-    <div className="bg-red-500">
-      <h1 className="text-yellow-400">Lets Go</h1>
-    </div>
+   <Navbar></Navbar>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return(
+        <>
+        <nav>
+            <span>Dev-Yuvi</span>
+            <ul>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Blogs</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+export default Navbar; 
