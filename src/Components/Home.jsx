228c259568ea9fda33034b8fe19e5753bcd3018c
Yuvraj <kingyuvi88@gmail.com>
2023-04-12 22:37:26 +0530

    Added text animation.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,46 @@
 import {IoPinSharp} from 'react-icons/io5';
-import TechStacks from './TechStacks';
 // import {yuvraj} from '../assets/YuvrajSingh.jpg';
 import {FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import TechStacks from './TechStacks';
+import { Typewriter} from 'react-simple-typewriter'
 
 //  import {SiJavascript, SiTailwindcss, SiMongodb, SiBootstrap} from 'react-icons/si';
 // import {DiNodejs} from 'react-icons/di'
 function Home(){
+
     return(
         <>
         <div className="h-auto w-auto flex flex-col pt-20 mb-8  ">
             <div className="flex mx-auto w-[50%] justify-around mb-8 ">
-                <div className="py-12">
+                {/* <div className="py-12">
                     <h1 className="text-5xl my-4 font-extrabold ">Front-End Developer</h1>
                     <p className="py-8 px-2 inline">Hi, I am <b>Yuvraj Singh</b>, a passionate front-end developer,<br/>
+                    based in <b>New Delhi, India</b><IoPinSharp className='text-red-700 inline font-bold'/>.
+                                
+
+                    </p>
+                    <p className='flex my-2 '>
+                    <a href="https://www.linkedin.com/in/yuvraj-singh-2942781a3/" className='inline p-1 text-3xl'><FaLinkedin/></a>
+                    <a href="https://github.com/Yuvraj883" className='inline p-1 text-3xl'><FaGithubSquare/> </a>
+                    </p>
+                </div> */}
+                <div className='py-16 w-[65%] pr-4'>
+                  <div>
+                  <h3 className='text-xl font-semibold inline mt-4'>Hi <span className='inline text-xl'>&#128075;</span>	</h3>
+                    {/* <img src={require('../assets/hand-emoji.png')} className="h-8 w-auto inline" alt='hand emoji'/> */}
+                  </div>
+                    <h1 className='inline text-lg font-semibold'>I am &nbsp; </h1>
+                    <span className='text-[#E5BA73] font-extrabold text-4xl h-16 mb-8 '>
+                    <Typewriter 
+                    words={[" a Front-End Developer", ' a Back-End Developer', ' a Full Stack Developer', ' Yuvraj Singh!!!']}
+                    loop={99999}
+                    cursor
+                    cursorStyle='|'
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                   /></span>
+                    <p className="py-8 px-2 inline"><br/>
                     based in <b>New Delhi, India</b><IoPinSharp className='text-red-700 inline font-bold'/>.
                                 
 
@@ -28,7 +56,7 @@ function Home(){
                 </div>
                 
             </div>
-           <TechStacks/>
+            <TechStacks/>
              
             {/* <div className='flex flex-col m-auto py-[-20] my-[-20] w-50%'>
                 <div className='flex'>
