0898eb046131b6c5a7924d8bfbe0865b67df3049
Yuvraj <kingyuvi88@gmail.com>
2023-04-12 13:53:33 +0530

    Designed Projects page.

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,21 @@
+import Project from "./Project";
 function Projects(){
     return(
-        <>
-        </>
+        <div className="flex flex-col my-16 ">
+        <h1 className="text-4xl text-cneter m-auto p-4">Projects</h1>
+
+        <div className="flex flex-wrap w-4/5 p-auto m-auto  justify-center items-center">
+
+        <Project className=""></Project>
+        <Project className=""></Project>
+        <Project className=""></Project>
+        <Project className=""></Project>
+        <Project className=""></Project>
+        <Project className=""></Project>
+       
+        </div>
+
+        </div>
     )
 }
 export default Projects; 
