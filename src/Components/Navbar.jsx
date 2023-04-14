b209799b6ed9e5c74714e29b9edc8a8cc98dfd18
Yuvraj <kingyuvi88@gmail.com>
2023-04-14 12:43:32 +0530

    Made the position of the navbar fixed.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { GiCrownOfThorns} from 'react-icons/gi'
 function Navbar(){
     return(
         <>
-        <nav className="flex text-red-950 ">
+        <nav className="flex text-red-950 bg-white fixed w-screen ">
             <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl"/> Dev-Yuvi</span>
             <ul className="flex ml-auto px-4 font-bold cursor-pointer">
                 <li className="m-2 p-2">Home</li>
