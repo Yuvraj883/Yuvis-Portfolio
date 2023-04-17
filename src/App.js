537ee853a6767f6153eebc58f5f08794f19b87fd
Yuvraj <kingyuvi88@gmail.com>
2023-04-17 23:37:46 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Home from './Components/Home';
 import About from './Components/About';
 // import TechStacks from './Components/TechStacks';
 import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App(){
   return(
     <>
     <div className="bg-[#f4f4f5]">
    <Navbar></Navbar>
-   <Home></Home>
+   <Home className='my-32'></Home>
    <About></About>
    <Projects></Projects>
    {/* <TechStacks/> */}
+   <Contact></Contact>
 
 
     </div>
