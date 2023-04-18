f748885e6114e5cdde518f492939a5c395264ff1
Yuvraj <kingyuvi88@gmail.com>
2023-04-18 23:25:56 +0530

    Updated Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,19 @@
 function Contact(){
     return(
-        <></>
+        <>
+        <form className="bg-white ">
+        <label className="block">Name</label>
+        <input/>
+        <label className="block">Email</label>
+        <input/>   
+        <label className="block">Subject</label>
+        <input/> 
+        <label className="block">Message</label>
+        <textbox/>
+
+
+        </form>
+        </>
     )
 }
 export default Contact; 
