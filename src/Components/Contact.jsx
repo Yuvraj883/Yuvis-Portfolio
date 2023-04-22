ad9775a462a051172ac6c58971ea6215b8332d6a
Yuvraj <kingyuvi88@gmail.com>
2023-04-22 23:43:29 +0530

    Updated Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,18 +1,16 @@
 function Contact(){
     return(
         <>
-        <form className="bg-white ">
-        <label className="block">Name</label>
-        <input/>
-        <label className="block">Email</label>
-        <input/>   
-        <label className="block">Subject</label>
-        <input/> 
-        <label className="block">Message</label>
-        <textbox/>
+        <div className="flex flex-col items-center bg-white">
+            <h1>Contact Me</h1>
+        <form className=" flex-col m-auto w-[40%] p-8">
+            <input placeholder="Full Name*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
+            <input placeholder="Email*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
+            <textarea placeholder="Message*" className="bg-slate-50 text-lg p-2 m-2 w-4/5"></textarea>
 
 
         </form>
+        </div>
         </>
     )
 }
