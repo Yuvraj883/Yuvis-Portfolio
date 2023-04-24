e5d464cee8653ea4f0f3994478fed38329a9088b
Yuvraj <kingyuvi88@gmail.com>
2023-04-24 23:30:51 +0530

    Connected to the @emailjs/browser

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,24 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import img from '../assets/contactUnDraw.svg';
 function Contact(){
+    const form = useRef();
+
+    const sendEmail = (e)=>{
+        e.preventDefault(); 
+        console.log("function called");
+
+        emailjs.sendForm('service_fljlucc', 'template_dihkdzp', form.current, '0_W1NUKR-qvMvdk7o')
+        .then((result)=>{
+            
+            console.log(result); 
+        }, (error)=>{
+            console.log(error);
+        }
+        )
+
+    }
+
     return(
         <>
         <div className="flex flex-col items-center bg-white w-[90%] shadow-lg m-auto px-12 py-8">
@@ -14,10 +33,10 @@ function Contact(){
                     <p className='w-4/5  text-slate-500 my-4 font-semibold'>My inbox is always open, wheather you have a queston, project idea or just want to say hello, 
                         I will try my best to get back to you!!
                     </p>
-                <form className=" flex-col my-4 ">
-                <input type='text' required placeholder="Full Name*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
-                <input type='email' required placeholder="Email*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
-                <textarea type='text' required placeholder="Message*" className="bg-slate-50 text-lg p-2 m-2 w-4/5 h-32"></textarea>
+                <form ref={form} className=" flex-col my-4 " onSubmit={sendEmail}>
+                <input name='from_name' type='text' required placeholder="Full Name*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
+                <input name='from_email' type='email' required placeholder="Email*" className="block bg-slate-50 p-2 text-lg m-2 w-4/5"/>
+                <textarea name='message' type='text' required placeholder="Message*" className="bg-slate-50 text-lg p-2 m-2 w-4/5 h-32"></textarea>
                 <button type='submit' className='bg-blue-500 block  px-6 font-bold text-white mx-2 py-2 rounded-md mt-2'>Send Message</button>
 
 
