83abdac12a15b17b0ff4c6c16c502351c586acf5
Yuvraj <kingyuvi88@gmail.com>
2023-06-06 01:36:21 +0530

    Made the component dynamic.

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,22 +1,27 @@
 import {BsGithub} from 'react-icons/bs'; 
 import {RiExternalLinkFill} from 'react-icons/ri'
-function Project(){
+function Project(props){
+ 
+
     return (
         <>
         <div className="flex flex-col w-1/4 m-3 shadow-md rounded-md p-4 bg-white">
             <img src={require('../assets/WhiteBoard.jpeg')} alt="Project Name"
             className="h-48 w-auto"/>
-            <h1 className='font-semibold text-xl'>White Board</h1>
+            <h1 className='font-semibold text-xl'>{props.props.title}</h1>
             <p>
-                <b>Description: </b>This is a whiteboard which can be used for teaching students, by drawing the shapes provided in it. 
+                <b>Description: </b> {props.props.description} 
             </p>
             <p className='p-1'>
-                <b>Tech Stack: </b> HTML, CSS, JS, Canva
+                <b>Tech Stack: </b> {props.props.techstack}
             </p>
         <div className='flex '>
-            <a href="https://github.com/Yuvraj883/LinkedInJsChallenge2" 
-            className='p-1 hover:text-blue-700 font-semibold text-md '>Code <BsGithub className='inline' /> </a>
-            <a href="https://github.com/Yuvraj883/LinkedInJsChallenge2"
+            <a href={props.props.github} 
+            className='p-1 hover:text-blue-700 font-semibold text-md ' 
+            target='blank'>Code <BsGithub className='inline' /> </a>
+            
+            <a href={props.props.live}
+            target='blank'
              className="p-1 hover:text-blue-700 font-semibold text-md ">Live Demo <RiExternalLinkFill className='inline'/> </a>
 
         </div>
