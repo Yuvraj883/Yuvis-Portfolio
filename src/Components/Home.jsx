7dfee165603e53e7a82cf746af8ab487cd290286
Yuvraj <kingyuvi88@gmail.com>
2023-06-06 03:07:41 +0530

    Added media query for small devices.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,57 +1,87 @@
-import {IoPinSharp} from 'react-icons/io5';
-import {FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { IoPinSharp } from 'react-icons/io5';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 import TechStacks from './TechStacks';
-import { Typewriter} from 'react-simple-typewriter'
+import { Typewriter } from 'react-simple-typewriter'
 
-function Home(){
+function Home() {
 
-    return(
+    return (
         <>
-        <div className="h-auto w-auto flex flex-col pt-20   ">
-            <div className="flex mx-auto w-[50%] justify-around mb-8 ">
-                
+            <div className="h-auto w-auto md:flex flex-col pt-20 hidden  ">
+                <div className="flex mx-auto w-[50%] justify-around mb-8 ">
+
+                    <div className='py-16 w-[65%] pr-4'>
+                        <div>
+                            <h3 className='text-xl font-semibold inline mt-4'>Hi <span className='inline text-xl'>&#128075;</span>	</h3>
+                        </div>
+                        <h1 className='inline text-lg font-semibold'>I am &nbsp; </h1>
+                        <span className='text-[#E5BA73] font-extrabold text-4xl h-16 mb-8 '>
+                            <Typewriter
+                                words={[" a Front-End Developer", ' a Back-End Developer', ' a Full Stack Developer', ' Yuvraj Singh!!!']}
+                                loop={99999}
+                                cursor
+                                cursorStyle='|'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                            /></span>
+                        <p className="py-8 px-2 inline"><br />
+                            based in <b>New Delhi, India</b><IoPinSharp className='text-red-700 inline font-bold' />.
+
+
+                        </p>
+                        <p className='flex my-2 '>
+                            <a href="https://www.linkedin.com/in/yuvraj-singh-2942781a3/" className='inline p-1 text-3xl'><FaLinkedin /></a>
+                            <a href="https://github.com/Yuvraj883" className='inline p-1 text-3xl'><FaGithubSquare /> </a>
+                        </p>
+                    </div>
+                    <div>
+                        <img src={require('../assets/image3.jpeg')}
+                            className="h-64 w-auto rounded-full" alt="Yuvraj Singh" />
+                    </div>
+
+                </div>
+
+
+                <TechStacks />
+
+
+            </div>
+            <div className='md:hidden flex flex-col jutify-center items-center p-4 m-2'>
+                <div>
+                    <img src={require('../assets/image3.jpeg')}
+                        className="h-64 w-auto rounded-full" alt="Yuvraj Singh" />
+                </div>
                 <div className='py-16 w-[65%] pr-4'>
-                  <div>
-                  <h3 className='text-xl font-semibold inline mt-4'>Hi <span className='inline text-xl'>&#128075;</span>	</h3>
-                  </div>
+                    <div>
+                        <h3 className='text-xl font-semibold inline mt-4'>Hi <span className='inline text-xl'>&#128075;</span>	</h3>
+                    </div>
                     <h1 className='inline text-lg font-semibold'>I am &nbsp; </h1>
                     <span className='text-[#E5BA73] font-extrabold text-4xl h-16 mb-8 '>
-                    <Typewriter 
-                    words={[" a Front-End Developer", ' a Back-End Developer', ' a Full Stack Developer', ' Yuvraj Singh!!!']}
-                    loop={99999}
-                    cursor
-                    cursorStyle='|'
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                   /></span>
-                    <p className="py-8 px-2 inline"><br/>
-                    based in <b>New Delhi, India</b><IoPinSharp className='text-red-700 inline font-bold'/>.
-                                
+                        <Typewriter
+                            words={[" a Front-End Developer", ' a Back-End Developer', ' a Full Stack Developer', ' Yuvraj Singh!!!']}
+                            loop={99999}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        /></span>
+                    <p className="py-8 px-2 inline"><br />
+                        based in <b>New Delhi, India</b><IoPinSharp className='text-red-700 inline font-bold' />.
+
 
                     </p>
                     <p className='flex my-2 '>
-                    <a href="https://www.linkedin.com/in/yuvraj-singh-2942781a3/" className='inline p-1 text-3xl'><FaLinkedin/></a>
-                    <a href="https://github.com/Yuvraj883" className='inline p-1 text-3xl'><FaGithubSquare/> </a>
+                        <a href="https://www.linkedin.com/in/yuvraj-singh-2942781a3/" className='inline p-1 text-3xl'><FaLinkedin /></a>
+                        <a href="https://github.com/Yuvraj883" className='inline p-1 text-3xl'><FaGithubSquare /> </a>
                     </p>
                 </div>
-                <div>
-                    <img src={require('../assets/image3.jpeg')}
-                     className="h-64 w-auto rounded-full" alt="Yuvraj Singh"/>
-                </div>
-                
-            </div>
-
-
-            <TechStacks/>
-             
 
             </div>
-            <div>
-          
-            </div>
 
-        
+
+
         </>
     )
 }
