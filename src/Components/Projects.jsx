fc6b5d2d4efb9d5a3954eeb15c0a63d00294d6a8
Yuvraj <kingyuvi88@gmail.com>
2023-06-06 01:35:54 +0530

    Added projects array.

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,17 +1,25 @@
 import Project from "./Project";
 function Projects(){
+    const projects = [
+        {   id:1,
+            title: 'White Board',
+            description:'It is a white board made using Canvas API, can be used for teaching students by drawing shapes and figures.',
+            techstack: 'HTML, CSS, JS, Canva',
+            github: 'https://github.com/Yuvraj883/LinkedInJsChallenge2', 
+            live: 'https://yuvis-whiteboard.netlify.app/'
+
+        }
+    ]
     return(
         <div className="flex flex-col my-16 ">
         <h1 className="text-4xl text-cneter m-auto p-4">Projects</h1>
 
         <div className="flex flex-wrap w-4/5 p-auto m-auto  justify-center items-center">
-
-        <Project className=""></Project>
-        <Project className=""></Project>
-        <Project className=""></Project>
-        <Project className=""></Project>
-        <Project className=""></Project>
-        <Project className=""></Project>
+            
+        {
+            projects.map((project, i)=> <Project props = {project} key={project.id} ></Project>)
+        }
+     
        
         </div>
 
