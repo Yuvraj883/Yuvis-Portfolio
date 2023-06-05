eabc964512810daaf97f1221120a83dd793dac4d
Yuvraj <kingyuvi88@gmail.com>
2023-06-06 03:43:24 +0530

    Added borders in the mobile view.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,48 +1,50 @@
 // import {GiCrownOfThorns} from 'react-icon/gi'; 
-import { GiCrownOfThorns} from 'react-icons/gi'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { GiCrownOfThorns } from 'react-icons/gi'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 import { useState } from 'react';
 
-function Navbar(){
+function Navbar() {
     const [nav, setNav] = useState(true);
 
-    const handleSetNav = ()=>{
+    const handleSetNav = () => {
         setNav(!nav);
     }
 
-    return(
+    return (
         <>
-        <nav className="md:flex justify-between text-red-950 bg-white fixed w-screen hidden ">
-            <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl"/> Dev-Yuvi</span>
-            <ul className="flex px-4 font-bold cursor-pointer">
-                <li className="m-2 p-2">Home</li>
-                <li className="m-2 p-2">About Me</li>
-                <li className="m-2 p-2">Projects</li>
-                <li className="m-2 p-2">Blogs</li>
-                <li className="m-2 p-2">Contact</li>
-            </ul>
-            
-        </nav>
-        <div className='flex justify-between items-center cursor-pointer  pr-2 md:hidden' onClick={handleSetNav}>
-        <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl"/> Dev-Yuvi</span>
-        <div>
-            {
-                nav? <AiOutlineMenu size={20}/> : <AiOutlineClose/>
-             }
-        </div>
+            <nav className="md:flex justify-between text-red-950 bg-white fixed w-screen hidden ">
+                <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi</span>
+                <ul className="flex px-4 font-bold cursor-pointer">
+                    <li className="m-2 p-2">Home</li>
+                    <li className="m-2 p-2">About Me</li>
+                    <li className="m-2 p-2">Projects</li>
+                    <li className="m-2 p-2">Blogs</li>
+                    <li className="m-2 p-2">Contact</li>
+                </ul>
 
-        </div>
-        <div className={!nav ? 'top-0 left-0 w-[60%] h-full fixed bg-black text-white z-10 p-4 ease-in-out duration-500' : 'fixed w-[60%] h-full p-4 top-0 left-[-100%] ease-in-out duration-500'}>
-        <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl"/> Dev-Yuvi</span>
-        
-        <ul className="flex flex-col ml-auto px-4 font-bold cursor-pointer">
-                <li className="m-2 p-2">Home</li>
-                <li className="m-2 p-2">About Me</li>
-                <li className="m-2 p-2">Projects</li>
-                <li className="m-2 p-2">Blogs</li>
-                <li className="m-2 p-2">Contact</li>
-            </ul>
-        </div>
+            </nav>
+
+            {/* Mobile View */}
+            <div className='flex justify-between items-center cursor-pointer  pr-2 md:hidden' onClick={handleSetNav}>
+                <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi</span>
+                <div>
+                    {
+                        nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />
+                    }
+                </div>
+
+            </div>
+            <div className={!nav ? 'top-0 left-0 w-[60%] h-full fixed bg-black text-white z-10 p-4 ease-in-out duration-500' : 'fixed w-[60%] h-full p-4 top-0 left-[-100%] ease-in-out duration-500'}>
+                <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi</span>
+
+                <ul className="flex flex-col ml-auto px-4 font-bold cursor-pointer">
+                    <li className="m-2 border-b border-[#ebca95] p-2">Home</li>
+                    <li className="m-2 border-b border-[#ebca95] p-2">About Me</li>
+                    <li className="m-2 border-b border-[#ebca95] p-2">Projects</li>
+                    <li className="m-2 border-b border-[#ebca95] p-2">Blogs</li>
+                    <li className="m-2 p-2">Contact</li>
+                </ul>
+            </div>
 
         </>
     )
