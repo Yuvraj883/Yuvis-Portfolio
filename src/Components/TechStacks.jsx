f32b83bdd11a535280ed05b5a31368e5cbcd1866
Yuvraj <kingyuvi88@gmail.com>
2023-06-08 03:02:11 +0530

    Updated style for small screen devices.

--- src/Components/TechStacks.jsx
+++ src/Components/TechStacks.jsx
@@ -187,7 +187,7 @@ function TechStacks() {
             {/* Mobile view */}
             <div className="md:hidden flex flex-col  w-full mx-auto my-8 justify-center items-center px-auto">
                 <h1 className="text-4xl  ">Tech Stacks</h1>
-                <div className="flex space-x-1 shadow-lg rounded-md p-2  font-bold text-lg w-[90%] bg-white m-4">
+                <div className="flex space-x-1 shadow-lg rounded-md p-2  font-bold text-xl w-[90%] bg-white m-4">
                     <button onClick={() => { setBackEnd(false); setOthers(false); setFrontEnd(true) }}
                         className={` ${frontEnd ? "bg-[#E5BA73] text-white py-1 px-2 rounded-md" : "hover:bg-[#f1f5f9] py-2 px-8 rounded-md  "}`}>Front-End</button>
                     <button onClick={() => { setFrontEnd(false); setOthers(false); setBackEnd(true) }}
