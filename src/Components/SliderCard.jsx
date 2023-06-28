c72371b2c66efad86c450459f6fd2bfa5e3bb721
Yuvraj <kingyuvi88@gmail.com>
2023-06-29 00:44:24 +0530

    Updated style

--- src/Components/SliderCard.jsx
+++ src/Components/SliderCard.jsx
@@ -5,25 +5,25 @@ function SliderCard(props, centerCard) {
 
     return (
         <>
-            <div className={props.centerCard==='true'?"md:flex hidden flex-col  w-[1/3] h-[125%] m-4 shadow-lg rounded-md p-4 bg-white" : "md:flex hidden flex-col w-[1/4] m-4 shadow-md rounded-md p-4 bg-white"}>
-                <div className='h-48 w-96 p-1 mx-auto'>  <img src={props?.props?.coverImg} alt="Project Cover Img"
+            <div className={props.centerCard==='true'?"md:flex hidden flex-col  w-auto h-[125%] m-4  shadow-2xl rounded-md p-4 bg-white" : "md:flex hidden flex-col w-[1/4] m-4 shadow-md rounded-md p-4 bg-white"}>
+                <div className='h-48 w-96  p-1 mx-auto'>  <img src={props?.props?.coverImg} alt="Project Cover Img"
                     className=" object-fit " />
                 </div>
-                <h1 className='font-semibold text-xl'>{props?.props?.title}</h1>
-                <p className={props.centerCard==='true'?'':'line-clamp-2'}>
+                <h1 className='font-semibold text-xl text-gray-700'>{props?.props?.title}</h1>
+                <p className={props.centerCard==='true'?'text-gray-600':'line-clamp-2 text-gray-600'}>
                     <b>Description: </b> {props?.props?.description}
                 </p>
-                <p className='p-1'>
+                <p className='p-1 text-gray-500'>
                     <b>Tech Stack: </b> {props?.props?.techstack}
                 </p>
                 <div className='flex '>
                     <a href={props?.props?.github}
-                        className='p-1 hover:text-blue-700 font-semibold text-md '
+                        className='p-1 hover:text-blue-700 text-gray-600 font-semibold text-md '
                         target='blank'>Code <BsGithub className='inline' /> </a>
 
                     <a href={props?.props?.live}
                         target='blank'
-                        className="p-1 hover:text-blue-700 font-semibold text-md ">Live Demo <RiExternalLinkFill className='inline' /> </a>
+                        className="p-1 hover:text-blue-700 font-semibold text-md  text-gray-600">Live Demo <RiExternalLinkFill className='inline' /> </a>
 
                 </div>
 
