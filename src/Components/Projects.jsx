aff5f89e1fbb0f193ac3be6b56813e9858f7ce1f
Yuvraj <devyuvraj883@gmail.com>
2024-04-02 23:36:53 +0530

    Update style

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
-import { RxDotFilled } from 'react-icons/rx'
+import React, { useState } from "react";
+import { RxDotFilled } from 'react-icons/rx';
 import SliderCard from "./SliderCard";
 import projects from './ProjectsArray';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
 function Projects() {
     const [index, setIndex] = useState(0);
+    const numProjects = projects.length;
 
-    const prevCard = () => {
-        const newIndex = index === 0 ? projects.length - 1 : index - 1;
-        setIndex(newIndex);
-    }
-
-    const nextCard = () => {
-        const newIndex = index === projects.length - 1 ? 0 : index + 1;
-        setIndex(newIndex);
-    }
+    const prevIndex = (index - 1 + numProjects) % numProjects;
+    const nextIndex = (index + 1) % numProjects;
 
     const goToSlide = (index) => {
         setIndex(index);
+    }
+
+    const handlePrevNext = (increment) => {
+        setIndex((index + increment + numProjects) % numProjects);
     }
 
     return (
@@ -26,28 +24,57 @@ function Projects() {
             <h1 className="text-4xl text-center m-auto p-4">Projects</h1>
             <div className="mx-auto bg-white p-6 shadow-lg md:block hidden group cursor-pointer justify-center items-center w-[80%]">
                 <div className="flex relative justify-center align-center p-6">
-                    <SliderCard centerCard='false' props={projects[index - 1 < 0 ? projects.length - 1 : index - 1]} key={projects[index - 1 < 0 ? projects.length - 1 : index - 1]} />
-                    <SliderCard centerCard='true' props={projects[index]} key={projects[index].id} />
-                    <SliderCard centerCard='false' props={projects[index + 1 > projects.length - 1 ? 0 : index + 1]} key={projects[index + 1 > projects.length - 1 ? 0 : index + 1].id} />
-                    <BsChevronCompactLeft onClick={prevCard} className="absolute hidden group-hover:block top-[50%] left-5 text-3xl -translate-x-0 -translate-y-[50%] duration-500 cursor-pointer bg-black/20 text-white rounded-full" size={30} />
-                    <BsChevronCompactRight onClick={nextCard} className="absolute hidden group-hover:block top-[50%] right-5 text-3xl cursor-pointer bg-black/20 -translate-x-0 -translate-y-[50%] duration-500 text-white rounded-full" size={30} />
+                    {[prevIndex, index, nextIndex].map((i, idx) => (
+                        <SliderCard
+                            key={projects[i].id}
+                            props={projects[i]}
+                            centerCard={i === index ? 'true' : 'false'}
+                            className={`transition-opacity duration-500 ${i === index ? 'opacity-100' : 'opacity-0'}`} // Apply opacity transition
+                        />
+                    ))}
+                    <BsChevronCompactLeft
+                        onClick={() => handlePrevNext(-1)}
+                        className="absolute hidden group-hover:block top-[50%] left-5 text-3xl -translate-x-0 -translate-y-[50%] duration-500  cursor-pointer bg-black/20 text-white rounded-full"
+                        size={30}
+                    />
+                    <BsChevronCompactRight
+                        onClick={() => handlePrevNext(1)}
+                        className="absolute hidden group-hover:block top-[50%] right-5 text-3xl cursor-pointer bg-black/20 -translate-x-0 -translate-y-[50%] duration-500 text-white rounded-full"
+                        size={30}
+                    />
                 </div>
                 <div className="flex flex-row justify-center items-center mt-4 text-black/20">
                     {projects.map((project, i) => (
-                        <RxDotFilled className={i === index ? "text-black" : ''} key={i} onClick={() => { goToSlide(i) }} />
+                        <RxDotFilled
+                            key={i}
+                            className={i === index ? "text-black" : ''}
+                            onClick={() => goToSlide(i)}
+                        />
                     ))}
                 </div>
             </div>
             {/* Mobile View */}
             <div className="md:hidden w-[80%] mx-auto bg-white shadow-lg p-8 block overflow-auto">
                 <div className="relative flex">
-                    <SliderCard props={projects[index]} centerCard='true' />
-                    <BsChevronCompactLeft onClick={prevCard} className="absolute top-[50%] left-1 text-3xl -translate-x-0 -translate-y-[50%] cursor-pointer bg-black/20 text-white rounded-full" size={30} />
-                    <BsChevronCompactRight onClick={nextCard} className="absolute top-[50%] right-1 text-3xl -translate-x-0 -translate-y-[50%] cursor-pointer bg-black/20 text-white rounded-full" size={30} />
+                    <SliderCard props={projects[index]} centerCard='true' className="transition-opacity duration-500 opacity-100" />
+                    <BsChevronCompactLeft
+                        onClick={() => handlePrevNext(-1)}
+                        className="absolute top-[50%] left-1 text-3xl -translate-x-0 -translate-y-[50%] cursor-pointer bg-black/20 text-white rounded-full"
+                        size={30}
+                    />
+                    <BsChevronCompactRight
+                        onClick={() => handlePrevNext(1)}
+                        className="absolute top-[50%] right-1 text-3xl -translate-x-0 -translate-y-[50%] cursor-pointer bg-black/20 text-white rounded-full"
+                        size={30}
+                    />
                 </div>
                 <div className="flex flex-row justify-center items-center mt-4 text-black/20">
                     {projects.map((project, i) => (
-                        <RxDotFilled className={i === index ? "text-black" : ""} key={i} onClick={() => { goToSlide(i) }} />
+                        <RxDotFilled
+                            key={i}
+                            className={i === index ? "text-black" : ''}
+                            onClick={() => goToSlide(i)}
+                        />
                     ))}
                 </div>
             </div>
