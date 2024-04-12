1245ab130886813513e11a0c917f649e30b8d8fd
Yuvraj <devyuvraj883@gmail.com>
2024-04-12 13:38:57 +0530

    Fixed sidebar not loading in mobile view issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,6 +12,8 @@ function Navbar() {
     setNav(!nav);
   };
 
+
+
   useEffect(() => {
     const rotateInterval = setInterval(() => {
       setRotateCounter((prevCounter) => prevCounter + 1);
@@ -57,64 +59,33 @@ function Navbar() {
         </ul>
       </nav>
 
-      {/* Mobile View */}
-      <div className="flex justify-between items-center cursor-pointer pr-2 md:hidden" onClick={handleSetNav}>
-        <animated.span style={rotateIcon} className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]">
+
+            {/* Mobile View */}
+            <div className='flex justify-between items-center cursor-pointer  pr-2 md:hidden' onClick={handleSetNav}>
+                {/* <span className="m-2 p-2 font-extrabold font-serif  text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline  text-3xl" /> Dev-Yuvi</span> */}
+                <animated.span style={rotateIcon} className="mt-2 mx-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]">
           <GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi
         </animated.span>
-        <div>
-          {nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose className="top-5 right-2 fixed" size={20} />}
-        </div>
-      </div>
+                <div>
+                    {
+                        nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose className='top-4 right-2 fixed ' size={20} />
+                    }
+                </div>
 
-      {/* Animated mobile navigation */}
-      <animated.div
-        style={{
-          ...fadeIn,
-          position: 'fixed',
-          width: '60%',
-          height: '100%',
-          backgroundColor: '#000',
-          color: '#fff',
-          padding: '1rem',
-          top: 0,
-          left: nav ? '100%' : 0,
-          zIndex: 10,
-        }}
-      >
-        <animated.span style={rotateIcon} className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]">
-          <GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi
-        </animated.span>
+            </div>
+            <div className={!nav ? 'top-0 left-0 w-[60%] h-full fixed bg-black text-white z-10 p-4 ease-in-out duration-500' : 'fixed w-[60%] h-full p-4 top-0 left-[-100%] ease-in-out duration-500'}>
+                <span className="m-2 p-2 font-extrabold font-serif text-xl text-[#E5BA73]"><GiCrownOfThorns className="inline text-3xl" /> Dev-Yuvi</span>
 
-        <animated.ul
-          style={{
-            ...fadeIn,
-            display: 'flex',
-            flexDirection: 'column',
-            marginLeft: 'auto',
-            paddingLeft: '1rem',
-            fontWeight: 'bold',
-            cursor: 'pointer',
-          }}
-        >
-          <Link to="home" smooth={true} duration={300} className="m-2 border-b border-[#ebca95] p-2">
-            Home
-          </Link>
-          <Link to="about" smooth={true} duration={300} className="m-2 border-b border-[#ebca95] p-2">
-            About Me
-          </Link>
-          <Link to="projects" smooth={true} duration={300} className="m-2 border-b border-[#ebca95] p-2">
-            Projects
-          </Link>
-          <Link to="experience" smooth={true} duration={300} className="m-2 border-b border-[#ebca95] p-2">
-            Experience
-          </Link>
-          <Link to="contact" smooth={true} duration={300} className="m-2 p-2">
-            Contact
-          </Link>
-        </animated.ul>
-      </animated.div>
-    </>
+                <ul className="flex flex-col ml-auto px-4 font-bold cursor-pointer">
+                    <Link to="home" smooth={true} duration={300} className="m-2 border-b border-[#ebca95] p-2">Home</Link>
+                    <Link to="about" smooth={true} duration={300}  className="m-2 border-b border-[#ebca95] p-2">About Me</Link>
+                    <Link to="projects" smooth={true} duration={300}  className="m-2 border-b border-[#ebca95] p-2">Projects</Link>
+                    <Link to="experience" smooth={true} duration={300}   className="m-2 border-b border-[#ebca95] p-2">Experience</Link>
+                    <Link to="contact" smooth={true} duration={300}  className="m-2 p-2">Contact</Link>
+                    </ul>  </div>
+
+</>
+
   );
 }
 
