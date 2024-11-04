f3893db1caddb3e907819d262e80a9d85a2f0e13
Yuvraj <devyuvraj883@gmail.com>
2024-11-04 15:37:42 +0530

    Reordered projects:

--- src/Components/ProjectsArray.js
+++ src/Components/ProjectsArray.js
@@ -8,9 +8,51 @@ const projects = [
      live:'https://easilyfamous.com/'
 
     },
-
     {
-      id: 11,
+      id: 8,
+      coverImg: '../images/PC-msit.png',
+      title: 'PC MSIT',
+      description: `Email: student@msit.in Password: 12345678
+      An initiative to make the on campus placement process easy and more accessible for the students of MSIT. Has admin panel as well for the Training Placement Officer.`,
+      techstack: 'MongoDB, ExpressJs, ReactJs, NodeJs, TailwindCSS ',
+      github: 'https://github.com/Yuvraj883/placement-cell',
+      live: 'https://placement-cell-iczn.onrender.com/home'
+
+  },
+
+
+{
+  id: 2,
+  coverImg: '../images/Blogify.png',
+  title: 'Blogify',
+  description: 'Blog app dedicated to posting Javascript related blogs. Has a login/signup and token based authentication system.',
+  techstack: 'MongoDB, Nodejs, ExpressJs, Ejs',
+  github: 'https://github.com/Yuvraj883/Blog-app',
+  live: 'https://js-world-navy.vercel.app/'
+
+},
+{
+  id: 11,
+  coverImg: '../images/cryptoTracker.jpeg',
+  title: 'Crypto Tracker',
+  description: 'This site lists the prices and other details of some of the most popular cryptos, also has the search functionality.',
+  techstack: 'ReactJs, TailwindCSS, CoinGecko API, fetchAPI',
+  github: 'https://github.com/Yuvraj883/Crypto-Tracker',
+  live: 'https://yuvis-crypto-tracker.netlify.app/'
+
+},
+{
+  id: 6,
+  coverImg: '../images/whiteBoard.jpeg',
+  title: 'White Board',
+  description: 'It is a white board made using Canvas API, can be used for teaching students by drawing shapes and figures.',
+  techstack: 'HTML, CSS, JS, Canva',
+  github: 'https://github.com/Yuvraj883/LinkedInJsChallenge2',
+  live: 'https://yuvis-whiteboard.netlify.app/'
+
+},
+    {
+      id: 19,
       coverImg: '../images/VishwaPublication.jpeg',
       title: 'Vital Vishwa Publications',
       description: 'Developed the website for Vital Vishwa Publication based on the given design.',
@@ -18,6 +60,7 @@ const projects = [
       github: 'https://github.com/Yuvraj883/Vishwa-Publication',
       live: 'https://vitalvishwapublications.netlify.app/'
     },
+
     {
         id: 10,
         coverImg: '../images/Nuloyal.jpeg',
@@ -29,50 +72,9 @@ const projects = [
 
     },
 
-    {
-        id: 9,
-        coverImg: '../images/Zomarzo.png',
-        title: 'Zomarzo: E-commerce platform',
-        description: 'A full stack e-commerce website, having features such as add to cart, search, filter, checkout, authentication etc. ',
-        techstack: 'NodeJs, ExpressJs, Javascript, HTML, CSS, TailwindCSS',
-        github: 'https://github.com/Yuvraj883/XArterian',
-        live: 'https://zamazor-xarterian.netlify.app/'
-
-    },
 
 
-    {
-        id: 8,
-        coverImg: '../images/PC-msit.png',
-        title: 'PC MSIT',
-        description: `Email: student@msit.in Password: 12345678
-        An initiative to make the on campus placement process easy and more accessible for the students of MSIT. Has admin panel as well for the Training Placement Officer.`,
-        techstack: 'MongoDB, ExpressJs, ReactJs, NodeJs, TailwindCSS ',
-        github: 'https://github.com/Yuvraj883/placement-cell',
-        live: 'https://placement-cell-iczn.onrender.com/home'
 
-    },
-    {
-      id: 11,
-      coverImg: '../images/cryptoTracker.jpeg',
-      title: 'Crypto Tracker',
-      description: 'This site lists the prices and other details of some of the most popular cryptos, also has the search functionality.',
-      techstack: 'ReactJs, TailwindCSS, CoinGecko API, fetchAPI',
-      github: 'https://github.com/Yuvraj883/Crypto-Tracker',
-      live: 'https://yuvis-crypto-tracker.netlify.app/'
-
-  },
-
-    {
-        id: 6,
-        coverImg: '../images/whiteBoard.jpeg',
-        title: 'White Board',
-        description: 'It is a white board made using Canvas API, can be used for teaching students by drawing shapes and figures.',
-        techstack: 'HTML, CSS, JS, Canva',
-        github: 'https://github.com/Yuvraj883/LinkedInJsChallenge2',
-        live: 'https://yuvis-whiteboard.netlify.app/'
-
-    },
 
     {
         id: 5,
@@ -84,36 +86,8 @@ const projects = [
         live: 'https://convrt-txt-to-speech.netlify.app/'
 
     },
-    // {
-    //     id: 4,
-    //     coverImg: './images/PasswordGenerator.jpeg',
-    //     title: 'Unique Password Generator',
-    //     description: 'It is a random password generator, which generates unique 15 characters long password using JS.',
-    //     techstack: 'HTML, CSS, JS',
-    //     github: 'https://github.com/Yuvraj883/Password-Generator',
-    //     live: 'https://newpasswordgenerator.netlify.app/'
 
-    // },
 
-    {
-        id: 3,
-        coverImg: '../images/TaskManager.jpeg',
-        title: 'Task Manager',
-        description: 'It is a practice website made while learning VueJS, it usage the local storage of the browser to store data.',
-        techstack: 'VueJs, LocalStorage',
-        github: 'https://github.com/Yuvraj883/Task-Manager',
-        live: 'https://github.com/Yuvraj883/Task-Manager'
-    },
-    {
-        id: 2,
-        coverImg: '../images/Blogify.png',
-        title: 'Blogify',
-        description: 'Blog app dedicated to posting Javascript related blogs. Has a login/signup and token based authentication system.',
-        techstack: 'MongoDB, Nodejs, ExpressJs, Ejs',
-        github: 'https://github.com/Yuvraj883/Blog-app',
-        live: 'https://js-world-navy.vercel.app/'
-
-    },
      {
         id: 1,
         coverImg: '../images/WebTemplate.jpeg',
@@ -124,7 +98,16 @@ const projects = [
         live: 'https://yuvraj883.github.io/Web-Template/'
 
     },
+    {
+      id: 9,
+      coverImg: '../images/Zomarzo.png',
+      title: 'Zomarzo: E-commerce platform',
+      description: 'A full stack e-commerce website, having features such as add to cart, search, filter, checkout, authentication etc. ',
+      techstack: 'NodeJs, ExpressJs, Javascript, HTML, CSS, TailwindCSS',
+      github: 'https://github.com/Yuvraj883/XArterian',
+      live: 'https://zamazor-xarterian.netlify.app/'
 
+  },
 
 
 ]
